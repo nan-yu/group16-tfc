2b9a500fa2dc265aee18ef69f36ad09e8b2b6744
Nan Yu <nanyu@google.com>
2020-07-08 16:15:42 -0700

    Change the region from us-east-2 to us-west-2

--- blueprint/variables.tf
+++ blueprint/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   type    = string
-  default = "us-east-2"
+  default = "us-west-2"
 }
 
 variable "team_name" {
--- core/variables.tf
+++ core/variables.tf
@@ -1,4 +1,4 @@
 variable "aws_region" {
   type    = string
-  default = "us-east-2"
+  default = "us-west-2"
 }
